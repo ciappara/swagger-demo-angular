3d86c80f2fb79a2638e2d9376fc9a251fa6b97d4
Clive Ciappara <clive.ciappara@fyorin.com>
2024-09-22 21:22:41 +0200

    Added weather forecast module

---
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -0,0 +1,18 @@
+//import { Client } from './../../api/api-client';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-forecast.component.html',
+  styleUrl: './weather-forecast.component.scss'
+})
+export class WeatherForecastComponent {
+
+    // constructor(private readonly clientSvc: Client) {
+    //     this.clientSvc.getWeatherForecast2();
+    // }
+
+}
